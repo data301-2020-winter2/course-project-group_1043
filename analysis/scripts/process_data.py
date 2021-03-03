29b56af3d8fad4cc83cce5cbb3b5dd8eebcfa48d
jribbink <jribbink@users.noreply.github.com>
2021-03-03 15:15:46 -0800

    Update process_data.py

--- analysis/scripts/process_data.py
+++ analysis/scripts/process_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 def load_and_process(path):
     df = (
         pd.read_csv(path,encoding='latin1')
-        .rename(columns=lambda x: x.strip())
-        .dropna()
+        .rename(columns=lambda x: x.strip())    # remove whitespace from header
+        .dropna()                               # drop any empty rows
     )
     return df
