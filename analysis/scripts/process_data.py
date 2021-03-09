556b6a96ce6ecda17600847e61807e19c39c920a
jribbink <jribbink@users.noreply.github.com>
2021-03-09 13:20:53 -0800

    Update process_data.py
    Implemented autoloading, processed datetime columns

--- analysis/scripts/process_data.py
+++ analysis/scripts/process_data.py
@@ -1,9 +1,15 @@
+#autoreload module to prevent IPython caching issues
+import autoreload
+autoreload
+
 import pandas as pd
 
 def load_and_process(path):
     df = (
         pd.read_csv(path,encoding='latin1')
-        .rename(columns=lambda x: x.strip())    # remove whitespace from header
-        .dropna()                               # drop any empty rows
+        .rename(columns=lambda x: x.strip())
+        .dropna()
+        .assign(launched = lambda x: pd.to_datetime(x['launched'], utc=True))
+        .assign(deadline = lambda x: pd.to_datetime(x['deadline'], utc=True))
     )
     return df
